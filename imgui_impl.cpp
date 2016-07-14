ec82099a3d0d50dde7997a3db20de080d7b916bd
Cristián Donoso <cristiandonosoc@gmail.com>
2016-07-13 23:30:05 -0400

    More initiazation copying
    This is so I understand what the integration is doing

--- imgui_impl.cpp
+++ imgui_impl.cpp
@@ -11,10 +11,12 @@ void RenderDrawLists(ImDrawData *drawData)
 
 }
 
-bool Init(SDL_Window *window)
+bool Init()
 {
     ImGuiIO& io = ImGui::GetIO();
-    io.KeyMap[ImGuiKey_Tab] = SDLK_TAB;                     // Keyboard mapping. ImGui will use those indices to peek into the io.KeyDown[] array.
+    // Keyboard mapping. 
+    // ImGui will use those indices to peek into the io.KeyDown[] array.
+    io.KeyMap[ImGuiKey_Tab] = SDLK_TAB;                     
     io.KeyMap[ImGuiKey_LeftArrow] = SDL_SCANCODE_LEFT;
     io.KeyMap[ImGuiKey_RightArrow] = SDL_SCANCODE_RIGHT;
     io.KeyMap[ImGuiKey_UpArrow] = SDL_SCANCODE_UP;
@@ -35,11 +37,43 @@ bool Init(SDL_Window *window)
     io.KeyMap[ImGuiKey_Z] = SDLK_z;
 
     io.RenderDrawListsFn = RenderDrawLists;
-
+    
+    // io.SetClipboardTextFn
+    // io.GetClipboardTextFn
+    
     return true;
 }
 
 static GLuint gFontTexture = 0;
+
+void CreateFontsTexture()
+{
+    ImGuiIO& io = ImGui::GetIO();
+    unsigned char *pixels;
+    int width, height;
+
+    // Load as RGBA 32-bits for OpenGL3 demo because it is 
+    // more likely to be compatible with user's existing shader.
+    io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height);
+
+    // Upload texture to graphics system
+    GLint lastTexture;
+    glGetIntegerv(GL_TEXTURE_BINDING_2D, &lastTexture);
+    glGenTextures(1, &gFontTexture);
+    glBindTexture(GL_TEXTURE_2D, gFontTexture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // Send the texture
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, 
+                 GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+
+    // Store the identief
+    io.Fonts->TexID = (void *)(intptr_t)gFontTexture;
+
+    // Restore the state
+    glBindTexture(GL_TEXTURE_2D, lastTexture);
+}
+
 static int gShaderHandle = 0;
 static int gVertexShaderHandle = 0;
 static int gFragmentShaderHandle = 0;
@@ -48,8 +82,11 @@ static int gAttribLocationTex = 0;
 static int gAttribLocationProjMtx = 0;
 static int gAttribLocationPosition = 0;
 static int gAttribLocationUV = 0;
+static int gAttribLocationColor = 0;
 
-
+static unsigned int gVBOHandle = 0;
+static unsigned int gVAOHandle = 0;
+static unsigned int gElementsHandle = 0;
 
 bool CreateDeviceObjects()
 {
@@ -60,22 +97,27 @@ bool CreateDeviceObjects()
     glGetIntegerv(GL_VERTEX_ARRAY_BINDING, &lastVertexArray);
 
     // We create the shaders
+    gShaderHandle = glCreateProgram();
+
+    // Vertex Shader
+    // Read
     std::ifstream vt("vertex_shader.vert");
     std::string str((std::istreambuf_iterator<char>(vt)),
                      std::istreambuf_iterator<char>());
     const GLchar *vertexShaderSource = str.c_str();
-
-    std::ifstream ft("fragment_shader.frag");
-    str = std::string((std::istreambuf_iterator<char>(ft)),
-                       std::istreambuf_iterator<char>());
-    const GLchar *fragmentShaderSource = str.c_str();
-
-    gShaderHandle = glCreateProgram();
+    // Compile/Create/Attach
     gVertexShaderHandle = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(gVertexShaderHandle, 1, &vertexShaderSource, 0);
     glCompileShader(gVertexShaderHandle);
     glAttachShader(gShaderHandle, gVertexShaderHandle);
 
+    // Fragment Shader
+    // Read
+    std::ifstream ft("fragment_shader.frag");
+    str = std::string((std::istreambuf_iterator<char>(ft)),
+                       std::istreambuf_iterator<char>());
+    const GLchar *fragmentShaderSource = str.c_str();
+    // Compile/Create/Attach
     gFragmentShaderHandle = glCreateShader(GL_FRAGMENT_SHADER);
     glShaderSource(gFragmentShaderHandle, 1, &fragmentShaderSource, 0);
     glCompileShader(gFragmentShaderHandle);
@@ -83,19 +125,44 @@ bool CreateDeviceObjects()
 
     glLinkProgram(gShaderHandle);
 
+    gAttribLocationTex      = glGetUniformLocation(gShaderHandle, "Texture");
+    gAttribLocationProjMtx  = glGetUniformLocation(gShaderHandle, "ProjMtx");
 
+    gAttribLocationPosition = glGetAttribLocation(gShaderHandle, "Position");
+    gAttribLocationUV       = glGetAttribLocation(gShaderHandle, "UV");
+    gAttribLocationColor    = glGetAttribLocation(gShaderHandle, "Color");
 
+    glGenBuffers(1, &gVBOHandle);
+    glBindBuffer(GL_ARRAY_BUFFER, gVBOHandle);
 
+    glGenBuffers(1, &gVAOHandle);
+    glBindVertexArray(gVAOHandle);
 
+    glGenBuffers(1, &gElementsHandle);
 
+    glEnableVertexAttribArray(gAttribLocationPosition);
+    glEnableVertexAttribArray(gAttribLocationUV);
+    glEnableVertexAttribArray(gAttribLocationColor);
 
-    gFragmentShaderHandle = glCreateShader(GL_FRAGMENT_SHADER);
-    gkS
-    
+#define OFFSETOF(TYPE, ELEMENT) ((size_t)&(((TYPE *)0)->ELEMENT))
+    glVertexAttribPointer(gAttribLocationPosition, 2, GL_FLOAT, GL_FALSE, sizeof(ImDrawVert), (GLvoid*)OFFSETOF(ImDrawVert, pos));
+    glVertexAttribPointer(gAttribLocationUV, 2, GL_FLOAT, GL_FALSE, sizeof(ImDrawVert), (GLvoid*)OFFSETOF(ImDrawVert, uv));
+    glVertexAttribPointer(gAttribLocationColor, 4, GL_UNSIGNED_BYTE, GL_TRUE, sizeof(ImDrawVert), (GLvoid*)OFFSETOF(ImDrawVert, col));
+#undef OFFSETOF
 
+    CreateFontsTexture();
 
+    // Restore modified OpenGL state
+    glBindTexture(GL_TEXTURE_2D, lastTexture);
+    glBindBuffer(GL_ARRAY_BUFFER, lastArrayBuffer);
+    glBindVertexArray(lastVertexArray);
 
+    return true;
 }
+
+static double gTime = 0.0f;
+static bool gMousePressed[3] = { false, false, false };
+static float gMouseWheel = 0.0f;
 
 void NewFrame(SDL_Window *window)
 {
@@ -104,4 +171,81 @@ void NewFrame(SDL_Window *window)
         CreateDeviceObjects();
     }
 
+    ImGuiIO& io = ImGui::GetIO();
+
+    // Setup display size (every frame to accommodate for window resizing)
+    int windowWidth, windowHeight;
+    int displayWidth, displayHeight;
+    SDL_GetWindowSize(window, &windowWidth, &windowHeight);
+    SDL_GL_GetDrawableSize(window, &displayWidth, &displayHeight);
+    io.DisplaySize = ImVec2((float)windowWidth, (float)windowHeight);
+    io.DisplayFramebufferScale = ImVec2((windowWidth > 0) ? ((float)displayWidth / windowWidth) : 0,
+                                        (windowHeight > 0) ? ((float)displayHeight / windowHeight) : 0);
+
+    // Setup the timestep
+    Uint32 time = SDL_GetTicks();
+    double currentTime = time / 1000.0;
+    io.DeltaTime = (gTime > 0.0) ? (float)(currentTime - gTime) : (float)(1.0f / 60.0f);
+    gTime = currentTime;
+
+    // Setup inputs
+    // (we already got mouse wheel, keyboard & characters from SDL_PollEvent)
+    
+    // Mouse position, in pixels (set to -1,-1 if no mouse / on another screen, etc.)
+    int mouseX, mouseY;
+    Uint32 mouseMask = SDL_GetMouseState(&mouseX, &mouseY);
+    if (!(SDL_GetWindowFlags(window) & SDL_WINDOW_MOUSE_FOCUS))
+    {
+        mouseX = -1;
+        mouseY = -1;
+    }
+    io.MousePos = ImVec2((float)mouseX, (float)mouseY);
+
+    io.MouseDown[0] = gMousePressed[0] || ((mouseMask & SDL_BUTTON(SDL_BUTTON_LEFT)) != 0);
+    io.MouseDown[1] = gMousePressed[1] || ((mouseMask & SDL_BUTTON(SDL_BUTTON_RIGHT)) != 0);
+    io.MouseDown[2] = gMousePressed[2] || ((mouseMask & SDL_BUTTON(SDL_BUTTON_MIDDLE)) != 0);
+
+    gMousePressed[0] = gMousePressed[1] = gMousePressed[2] = false;
+
+    io.MouseWheel = gMouseWheel;
+    gMouseWheel = 0.0f;
+
+    // Hide OS cursor if ImGui is drawing it
+    SDL_ShowCursor(io.MouseDrawCursor ? 0 : 1);
+
+    // Start the frame
+    ImGui::NewFrame();
+}
+
+
+void InvalidateDeviceObjects()
+{
+    if (gVAOHandle) { glDeleteVertexArrays(1, &gVAOHandle); }
+    if (gVBOHandle) { glDeleteBuffers(1, &gVBOHandle); }
+    if (gElementsHandle) { glDeleteBuffers(1, &gElementsHandle); }
+
+    gVAOHandle = gVBOHandle = gElementsHandle = 0;
+
+    glDetachShader(gShaderHandle, gVertexShaderHandle);
+    glDeleteShader(gVertexShaderHandle);
+    gVertexShaderHandle = 0;
+
+    glDetachShader(gShaderHandle, gFragmentShaderHandle);
+    glDeleteShader(gFragmentShaderHandle);
+    gFragmentShaderHandle = 0;
+
+    glDeleteProgram(gShaderHandle);
+
+    if (gFontTexture)
+    {
+        glDeleteTextures(1, &gFontTexture);
+        ImGui::GetIO().Fonts->TexID = 0;
+        gFontTexture = 0;
+    }
+}
+
+void Shutdown()
+{
+    InvalidateDeviceObjects();
+    ImGui::Shutdown();
 }
--- sdl_main.cpp
+++ sdl_main.cpp
@@ -14,7 +14,7 @@ int main(int, char **)
         return -1;
     }
 
-    // Setup window
+    // Initial OpenGL attributes
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
@@ -22,24 +22,25 @@ int main(int, char **)
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+
+    // TODO(Cristian): Is this necessary?
     SDL_DisplayMode current;
     SDL_GetCurrentDisplayMode(0, &current);
+
+    // We create the window
     SDL_Window *window = SDL_CreateWindow("ImGui SDL2+OpenGL3 example", 
                                           SDL_WINDOWPOS_CENTERED, 
                                           SDL_WINDOWPOS_CENTERED, 
                                           1280, 720, 
                                           SDL_WINDOW_OPENGL|SDL_WINDOW_RESIZABLE);
-    SDL_GLContext glcontext = SDL_GL_CreateContext(window);
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
 
     // We init the open gl extensions
     gl3wInit();
-
-    Init(window);
-
+    Init();
     
-    ImVec4 clearColor = ImColor(114, 114, 154);
-
     // "Game loop"
+    ImVec4 clearColor = ImColor(114, 114, 154);
     bool running = true;
     while (running)
     {
@@ -55,16 +56,25 @@ int main(int, char **)
 
         NewFrame(window);
 
+        // Show simple window
+        // Tip: if we don't call ImGui::Begin()/ImGui::End() 
+        // the widgets appears in a window automatically called "Debug"
+        ImGui::Text("Hello World!");
 
+        // Rendering
+        ImGuiIO& io = ImGui::GetIO();
+        glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+        glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui::Render();
+        SDL_GL_SwapWindow(window);
     }
 
+    // Cleanup
+    Shutdown();
+    SDL_GL_DeleteContext(glContext);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
-
-
-
-
-
-
-
-
+    return 0;
 }
